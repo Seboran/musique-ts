e1fd79ad85b336e25bc1fbd179d4227b79a584d7
Nirina Rabeson <nirina.rabeson@zenika.com>
2023-05-17 18:48:05 +0200

    Suppression extend inutile note

--- src/model/Note.ts
+++ src/model/Note.ts
@@ -1,9 +1,6 @@
-import { NoeudModel } from 'transpilation-ts'
 import DureeNote from './DureeNote'
 import HauteurNote from './HauteurNote'
 
-export default class Note extends NoeudModel {
-  constructor(public hauteur: HauteurNote, public duree: DureeNote) {
-    super()
-  }
+export default class Note {
+  constructor(public hauteur: HauteurNote, public duree: DureeNote) {}
 }
